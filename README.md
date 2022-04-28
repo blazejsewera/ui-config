244d4c36903d5d1f63f4636d961c793cf152fd0a
Blazej Sewera <code@sewera.dev>
2022-04-28 12:35:39 +0200

    feat: add configs

---
+++ README.md
@@ -0,0 +1,22 @@
+# UI config
+
+Configuration files for ui projects.
+
+## Usage
+
+```sh
+git submodule add https://github.com/blazejsewera/ui-config ./ui/config
+```
+
+or
+
+```sh
+git submodule add git@github.com:blazejsewera/ui-config.git ./ui/config
+```
+
+where `./ui/config` is the path in your repository.
+
+Then, you have to reference those configs from the ui project root.
+You will find example files in `.ref` directory.
+
+Remember to clone your repositories with `--recursive` flag when using `ui-config`.
---
+++ tailwind.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
